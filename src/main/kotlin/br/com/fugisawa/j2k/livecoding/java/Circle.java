5ead6f48208837111e0a9e1900b0d74275f093cd
Lucas Fugisawa <lucasfugisawa@gmail.com>
2023-10-28 12:42:02 -0300

    Initial commit.

---
+++ src/main/kotlin/br/com/fugisawa/j2k/livecoding/java/Circle.java
@@ -0,0 +1,45 @@
+package br.com.fugisawa.j2k.livecoding.java;
+
+import java.util.Objects;
+
+public class Circle {
+    private final Point center;
+    private final int radius;
+
+    public Circle(Point center, int radius) {
+        Objects.requireNonNull(center, "center must not be null");
+        this.center = center;
+        this.radius = radius;
+    }
+
+    public Circle(Point center) {
+        this(center, 1);
+    }
+
+    public Point getCenter() { return center; }
+    public int getRadius() { return radius; }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Circle circle = (Circle) o;
+        if (radius != circle.radius) return false;
+        return Objects.equals(center, circle.center);
+    }
+
+    @Override
+    public int hashCode() {
+        int result = center != null ? center.hashCode() : 0;
+        result = 31 * result + radius;
+        return result;
+    }
+
+    @Override
+    public String toString() {
+        return "Circle{" +
+                "center=" + center +
+                ", radius=" + radius +
+                '}';
+    }
+}
---
+++ src/main/kotlin/br/com/fugisawa/j2k/livecoding/java/Point.java
@@ -0,0 +1,42 @@
+package br.com.fugisawa.j2k.livecoding.java;
+
+public class Point {
+    private final int x;
+    private final int y;
+
+    public Point(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public int getX() { return x; }
+    public int getY() { return y; }
+
+    public double distanceTo(Point point) {
+        return Math.sqrt(Math.pow(point.x - x, 2) + Math.pow(point.y - y, 2));
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Point point = (Point) o;
+        if (x != point.x) return false;
+        return y == point.y;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = x;
+        result = 31 * result + y;
+        return result;
+    }
+
+    @Override
+    public String toString() {
+        return "Point{" +
+                "x=" + x +
+                ", y=" + y +
+                '}';
+    }
+}
